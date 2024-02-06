a11c8efb7651dd8c2921f515acaa1c7a17fc88a0
Jahir-CR <javaca24@hotmail.com>
2024-02-06 14:41:32 -0600

    Corrections added after code review

--- src/Components/CountryInfo.jsx
+++ src/Components/CountryInfo.jsx
@@ -3,34 +3,24 @@ import api from "../API/Countries";
 import "./styles.css";
 
 const CountryInfo = () => {
-  const [countries, setCountry] = useState("");
-  const [countryCapital, setCountryCapital] = useState("");
-  const [countryImage, setCountryImage] = useState("");
-  let count = 0;
-
+  const [countries, setCountry] = useState({name: "", capital: "", image: ""}); 
+  
   const GetCountry = async () => {
     const countryName = document.querySelector(".country").value;
     const countriesNames = await api
       .get(`/${countryName}`)
       .then((res) => {
-        //obtener el nombre del pais
         const country = res.data[0].name.common;
-        setCountry(country);
-
-        //obtener la capital del pais
-        const capital = res.data[0].capital[0];
-        setCountryCapital(capital);
-
-        //obtener la bandera del pais
-        const image = res.data[0].flags.png;
-        setCountryImage(image);
+        const imageURL = res.data[0].flags.png;
+        const capitalName = res.data[0].capital[0];
+        setCountry({...countries, name:country, capital:capitalName, image:imageURL});   
       })
       .catch((err) => console.log(err));
   };
 
   console.log(countries);
-  console.log(countryCapital);
-  console.log(countryImage);
+//   console.log(countries.capital);
+//   console.log(countries.image);
 
   return (
     <>
@@ -41,20 +31,19 @@ const CountryInfo = () => {
           placeholder="Enter country name"
         ></input>
         <button className="button" onClick={GetCountry}>
-          {" "}
-          Click{" "}
+          Click
         </button>
         <div className="home-container">
           <ul>
-            {countries.length > 0 && (
-              <li key = {count++}>
+            {
+              <li>
                 <div>
-                  <p>País: {countries}</p>
-                  <p>Capital: {countryCapital}</p>
-                  <img className="country_img" src={countryImage}></img>
+                  <p>País: {countries.name}</p>
+                  <p>Capital: {countries.capital}</p>
+                  <img className = "country_img" src={countries.image}></img>
                 </div>
               </li>
-            )}
+            }
           </ul>
         </div>
       </div>
